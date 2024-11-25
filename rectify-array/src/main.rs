6d4bd8b72326459b814a910d65f3b52e74cae284
JaredS129 <jaredkevin.stevenson@gmail.com>
2024-11-25 09:04:55 +0000

    mutate the array with random f32 within range -1 to 1

--- rectify-array/src/main.rs
+++ rectify-array/src/main.rs
@@ -6,17 +6,18 @@ fn print_buffer(bfr: &Vec<f32>) {
     }
 }
 
-fn rand_fill(bfr: &Vec<f32>) {
+fn rand_fill(bfr: &mut Vec<f32>) {
     let mut range = rand::thread_rng();
-    for i in bfr.iter() {
-        // generate a random float between -1 and 1
+    for i in bfr.iter_mut() {
         let random_float: f32 = range.gen_range(-1.0..1.0);
-        bfr[i] = random_float;
+        *i = random_float;
     }
 }
 fn main() {
     const SAMPLE_RATE: usize = 10;
     let initial_buffer: [f32; SAMPLE_RATE] = [0.0; SAMPLE_RATE];
-    let buffer: Vec<f32> = Vec::from(initial_buffer);  
+    let mut buffer: Vec<f32> = Vec::from(initial_buffer);  
     print_buffer(&buffer); 
+    rand_fill(&mut buffer);
+    print_buffer(&buffer);
 }
