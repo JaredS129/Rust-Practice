835494fe57cac2e88dd64dc54b92cf1caeda84d8
Naircol <matt.hennessy@protonmail.com>
2024-12-01 17:14:08 +1300

    Modified main function to calculate mulltiples of a user input array. Could refactor into seperate function

--- print-multiples/src/main.rs
+++ print-multiples/src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use std::vec;
+
+
 fn main() {
-    println!("Printed multiples");
+    println!("How many multiples would you like to calculate?: ");
+    let mut size_input = String::new();
+    io::stdin().read_line(&mut size_input).expect("Failed to read line");
+
+    let size: usize = size_input.trim().parse().expect("Please type a number");
+
+    println!("What number would you like to multiply?: ");
+
+    let mut num = String::new();
+    io::stdin().read_line(&mut num).expect("Failed to read line");
+
+    let num: usize = num.trim().parse().expect("Please type a number");
+    let mut array = vec![];();
+
+    for i in 0..size {
+        let element: usize = num * (i + 1);
+        array.push(element);
+    }
+    println!("The first {} multiples of {} are {:?}", size, num, array);
 }
+
